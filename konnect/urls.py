68fd65160c78e74bcd92a70e18c5a619ab025824
Jyotishmoy Deka <93263133+Jyotishmoy12@users.noreply.github.com>
2025-07-28 23:53:30 +0530

    Update urls.py

--- konnect/urls.py
+++ konnect/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.urls import views as auth_views
-from django.shortcuts import redirect  
+from django.shortcuts import redirect
 
 urlpatterns = [
     path("", lambda request: redirect("/tweet/", permanent=False)),
